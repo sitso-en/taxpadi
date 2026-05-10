8ac4f4d6bf6ffeefaaf24b628265ece6d2b91c45
sitso-en <sitso.nkrumah@gmail.com>
2026-05-10 23:45:49 +0000

    Add Spring Boot application entry point

---
+++ backend/api/src/main/java/com/taxpadi/api/TaxPadiApplication.java
@@ -0,0 +1,13 @@
+package com.taxpadi.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxPadiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxPadiApplication.class, args);
+	}
+
+}
