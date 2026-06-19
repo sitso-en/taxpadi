a06db43887d52839ee375c1c2c1c90bae474b10b
sitso-en <sitso.nkrumah@gmail.com>
2026-06-19 13:19:28 +0000

    Fixed VaultTransactionRepository to use UUID and query by SavingsVault instead of a raw ID

--- backend/jeffery/repository/VaultTransactionRepository.java
+++ backend/jeffery/repository/VaultTransactionRepository.java
@@ -1,8 +1,16 @@
-package com.taxpadi.repository;
-import com.taxpadi.entity.VaultTransaction;
+package com.taxpadi.api.repository;
+
+import com.taxpadi.api.model.SavingsVault;
+import com.taxpadi.api.model.VaultTransaction;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-public interface VaultTransactionRepository extends JpaRepository<VaultTransaction,Long> {
-    Page<VaultTransaction> findByVaultIdOrderByCreatedAtDesc(Long vaultId, Pageable pageable);
+
+import java.util.List;
+import java.util.UUID;
+
+public interface VaultTransactionRepository extends JpaRepository<VaultTransaction, UUID> {
+    Page<VaultTransaction> findByVaultOrderByCreatedAtDesc(SavingsVault vault, Pageable pageable);
+
+    List<VaultTransaction> findByVault(SavingsVault vault);
 }
