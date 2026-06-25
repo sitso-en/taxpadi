047fdadb4d76cc8d70b77273ec512412181d4940
sitso-en <sitso.nkrumah@gmail.com>
2026-06-25 05:58:25 +0000

    Add PaymentMethod constant

---
+++ backend/api/src/main/java/com/taxpadi/api/constant/PaymentMethod.java
@@ -0,0 +1,10 @@
+package com.taxpadi.api.constant;
+
+public final class PaymentMethod {
+    public static final String MOMO      = "momo";
+    public static final String BANK_CARD = "bank_card";
+    public static final String USSD      = "ussd";
+    public static final String VAULT     = "vault";
+
+    private PaymentMethod() {}
+}
